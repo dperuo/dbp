09f557badb2bebfb1508fa3b66a754ef667a5b86
Derek Peruo <derek@derekperuo.net>
2015-11-07 14:58:14 -0500

    Add Taming of A Shrew

--- assets/js/credits.js
+++ assets/js/credits.js
@@ -3,8 +3,9 @@ var $work = $("#work");
 $work.empty();
 
 $(document).ready(function(){
-  
+
   var credits = [{title: "Much Ado About Nothing", role: "Benedick"},
+                 {title: "The Taming of A Shrew", role: "Ferando"},
                  {title: "Henry V", role: "Fluellen"},
                  {title: "Life&rsquo;s a Dream", role: "Basilio"},
                  {title: "Iphigenia in Tauris", role: "Arkas"},
@@ -13,21 +14,20 @@ $(document).ready(function(){
                  {title: "Machinal", role: "Prosecuting Attorney"},
                  {title: "The Lady from Dubuque", role: "Oscar"},
                  {title: "Picasso at the Lapin Agile", role: "Gaston"},
-                 {title: "Snow Angel", role: "Doofus"}
                 ];
 
   var allCredits = [];
-  
+
   $.each(credits, function(i, credit){
     if ( i === 0){
-      allCredits.push('<div class="col-lg-4 col-md-4 col-xs-6"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
+      allCredits.push('<div class="col-sm-4 col-xs-12"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
     } else if (i % 2 == 0){
-      allCredits.push('<div class="col-lg-4 col-md-4 col-xs-6 col-lg-offset-3"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
+      allCredits.push('<div class="col-sm-4 col-xs-12 col-lg-offset-3"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
     } else {
-      allCredits.push('<div class="col-lg-4 col-md-4 col-xs-6"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
+      allCredits.push('<div class="col-sm-4 col-xs-12"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
     };
   });
-                   
+
   $work.html(allCredits);
 
 });
