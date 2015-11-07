b4c5efb7bdf0db99f99101db284f40adc6f99ce4
Derek Peruo <derek@derekperuo.net>
2015-11-07 15:00:02 -0500

    use array#join()

--- assets/js/credits.js
+++ assets/js/credits.js
@@ -1,33 +1,33 @@
-var $work = $("#work");
+var $work = $("#work")
 
-$work.empty();
+$work.empty()
 
 $(document).ready(function(){
 
   var credits = [{title: "Much Ado About Nothing", role: "Benedick"},
                  {title: "The Taming of A Shrew", role: "Ferando"},
                  {title: "Henry V", role: "Fluellen"},
-                 {title: "Life&rsquo;s a Dream", role: "Basilio"},
+                 {title: "Life&rsquos a Dream", role: "Basilio"},
                  {title: "Iphigenia in Tauris", role: "Arkas"},
                  {title: "Kosi Dasa", role: "Papacacia"},
                  {title: "Man of La Mancha", role: "Pedro"},
                  {title: "Machinal", role: "Prosecuting Attorney"},
                  {title: "The Lady from Dubuque", role: "Oscar"},
                  {title: "Picasso at the Lapin Agile", role: "Gaston"},
-                ];
+                ]
 
-  var allCredits = [];
+  var allCredits = []
 
   $.each(credits, function(i, credit){
     if ( i === 0){
-      allCredits.push('<div class="col-sm-4 col-xs-12"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
+      allCredits.push(['<div class="col-sm-4 col-xs-12"><p><t>', credit.title, '</t><br/>', credit.role, '</p></div>'].join(''))
     } else if (i % 2 == 0){
-      allCredits.push('<div class="col-sm-4 col-xs-12 col-lg-offset-3"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
+      allCredits.push(['<div class="col-sm-4 col-xs-12 col-lg-offset-3"><p><t>', credit.title, '</t><br/>', credit.role, '</p></div>'].join(''))
     } else {
-      allCredits.push('<div class="col-sm-4 col-xs-12"><p><t>' + credit.title + '</t><br/>' + credit.role + '</p></div>')
-    };
-  });
+      allCredits.push(['<div class="col-sm-4 col-xs-12"><p><t>', credit.title, '</t><br/>', credit.role, '</p></div>'].join(''))
+    }
+  })
 
-  $work.html(allCredits);
+  $work.html(allCredits)
 
-});
+})
