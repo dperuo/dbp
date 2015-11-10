9a33e40f6139d55783a045c330c8c39f66249359
Derek Peruo <derek@derekperuo.net>
2015-11-10 00:14:39 -0500

    housecleaning JavaScript

--- assets/js/credits.js
+++ assets/js/credits.js
@@ -4,24 +4,23 @@ $work.empty()
 
 $(document).ready(function(){
 
-  var credits = [{title: "Much Ado About Nothing", role: "Benedick"},
-                 {title: "The Taming of A Shrew", role: "Ferando"},
-                 {title: "Henry V", role: "Fluellen"},
-                 {title: "Life&rsquos a Dream", role: "Basilio"},
-                 {title: "Iphigenia in Tauris", role: "Arkas"},
-                 {title: "Kosi Dasa", role: "Papacacia"},
-                 {title: "Man of La Mancha", role: "Pedro"},
-                 {title: "Machinal", role: "Prosecuting Attorney"},
-                 {title: "The Lady from Dubuque", role: "Oscar"},
-                 {title: "Picasso at the Lapin Agile", role: "Gaston"},
-                ]
+  var credits = [
+    {title: "Much Ado About Nothing", role: "Benedick"},
+    {title: "The Taming of A Shrew", role: "Ferando"},
+    {title: "Henry V", role: "Fluellen"},
+    {title: "Life\'s a Dream", role: "Basilio"},
+    {title: "Iphigenia in Tauris", role: "Arkas"},
+    {title: "Kosi Dasa", role: "Papacacia"},
+    {title: "Man of La Mancha", role: "Pedro"},
+    {title: "Machinal", role: "Prosecuting Attorney"},
+    {title: "The Lady from Dubuque", role: "Oscar"},
+    {title: "Picasso at the Lapin Agile", role: "Gaston"},
+  ]
 
   var allCredits = []
 
-  $.each(credits, function(i, credit){
-    if ( i === 0){
-      allCredits.push(['<div class="col-sm-4 col-xs-12"><p><t>', credit.title, '</t><br/>', credit.role, '</p></div>'].join(''))
-    } else if (i % 2 == 0){
+  credits.forEach(function(credit, index, array) {
+    if (index > 0 && index % 2 === 0) {
       allCredits.push(['<div class="col-sm-4 col-xs-12 col-lg-offset-3"><p><t>', credit.title, '</t><br/>', credit.role, '</p></div>'].join(''))
     } else {
       allCredits.push(['<div class="col-sm-4 col-xs-12"><p><t>', credit.title, '</t><br/>', credit.role, '</p></div>'].join(''))
